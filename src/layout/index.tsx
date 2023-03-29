cdd7c312d35ec6e8fd9fa943103c90ad8b451d9c
Vinícius Cabral <96541031+vini-cabral@users.noreply.github.com>
2023-03-29 18:56:20 -0300

    Structure for the modal inserted

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -2,11 +2,15 @@ import { ReactNode } from "react";
 // My Assets:
 import GlobalStyle from "@/styles/globals";
 import { Footer, Header } from "@/templates";
+import { BackGroundModal } from "@/components";
+import useAppContext from "@/hooks/useAppContext";
 
 const Layout = ({ children }: { children: ReactNode }) => {
+  const { showModalGob } = useAppContext();
   return (
     <>
-      <GlobalStyle hideBodyScrollbar={false} />
+      <GlobalStyle hideBodyScrollbar={showModalGob} />
+      {showModalGob && <BackGroundModal>Loading...</BackGroundModal>}
       <Header />
       {children}
       <Footer />
