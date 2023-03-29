4e77e7157f11b07d013dff4b21acdc813cf1ee13
Vinícius Cabral <96541031+vini-cabral@users.noreply.github.com>
2023-03-29 18:53:51 -0300

    Added method to hide the modal

--- src/components/BackGroundModal/index.tsx
+++ src/components/BackGroundModal/index.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 /** My Assets */
 import styles from "@/components/BackGroundModal/styles.module.sass";
+import useAppContext from "@/hooks/useAppContext";
 
 export default function BackGroundModal({
   children,
 }: {
   children?: React.ReactNode;
 }) {
-  return <div className={styles["back-ground-modal"]}>{children}</div>;
+  const { handleHideModalGob } = useAppContext();
+  return (
+    <div onClick={handleHideModalGob} className={styles["back-ground-modal"]}>
+      {children}
+    </div>
+  );
 }
