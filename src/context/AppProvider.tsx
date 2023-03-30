254cfd0057af2680740055846aee0248694ae861
Vinícius Cabral <96541031+vini-cabral@users.noreply.github.com>
2023-03-30 08:39:51 -0300

    Added global state for feedback during fetch data

--- src/context/AppProvider.tsx
+++ src/context/AppProvider.tsx
@@ -12,22 +12,32 @@ const initState: {
   showModalGob: boolean;
   handleShowModalGob: () => void;
   handleHideModalGob: () => void;
+  isLoadingGob: boolean;
+  handleIsLoadingGob: (bool: boolean) => void;
 } = {
   showModalGob: false,
   handleShowModalGob: () => {},
   handleHideModalGob: () => {},
+  isLoadingGob: false,
+  handleIsLoadingGob: () => {},
 };
 
 export const AppContext = createContext<typeof initState>(initState);
 
 export default function AppProvider({ children }: { children: ReactNode }) {
   const [showModal, setShowModal] = useState(initState.showModalGob);
+  const [isLoading, setIsLoading] = useState(initState.isLoadingGob);
   return (
     <AppContext.Provider
       value={{
         showModalGob: useMemo(() => showModal, [showModal]),
         handleShowModalGob: useCallback(() => setShowModal(() => true), []),
         handleHideModalGob: useCallback(() => setShowModal(() => false), []),
+        isLoadingGob: useMemo(() => isLoading, [isLoading]),
+        handleIsLoadingGob: useCallback(
+          (bool: boolean) => setIsLoading(() => bool),
+          []
+        ),
       }}
     >
       <ScreenModeProvider>{children}</ScreenModeProvider>
