7bff09842e41715ffe9dcb2ec34e20bf49ad9c67
Vinícius Cabral <96541031+vini-cabral@users.noreply.github.com>
2023-03-30 10:29:19 -0300

    Added environment variables of the RANDOM USER GENERATOR api

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,14 +1,44 @@
 import { Ubuntu } from "next/font/google";
-// My Assets:
+/** My Assets: */
 import { LinkType, ScreenModeType } from "@/types";
 
+/** Google Fonts */
 export const PUB_GOOGLE_FONTS_UBUNTU = Ubuntu({
   subsets: ["latin"],
   weight: ["300", "400", "500", "700"],
 });
 
-export const PUB_SCREEN_MODE: ScreenModeType = "light"
+/** Api Random User Generator: https://randomuser.me/ */
+export const PUB_RANDOMUSER_URL = "https://randomuser.me/api/1.4/";
 
+export const PUB_RANDOMUSER_NAT = [
+  "AU",
+  "BR",
+  "CA",
+  "CH",
+  "DE",
+  "DK",
+  "ES",
+  "FI",
+  "FR",
+  "GB",
+  "IE",
+  "IN",
+  "IR",
+  "MX",
+  "NL",
+  "NO",
+  "NZ",
+  "RS",
+  "TR",
+  "UA",
+  "US",
+];
+
+/** App Screen Mode (Init) */
+export const PUB_SCREEN_MODE: ScreenModeType = "light";
+
+/** App Links */
 export const PUB_LINK_AUTHOR: LinkType = {
   name: "Vinícius Cabral",
   href: "https://github.com/vini-cabral/",
